555924d625aa46f821824883e0d2cc7b48a619be
thealiane09 <inalealtia15@gmail.com>
2026-02-09 01:16:36 +0800

    Done with Hello World Demo

--- react-native-getting-started/app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import HelloWorldDemo from "../../HelloWorldDemo";
-
-export default function HomeScreen() {
-  return <HelloWorldDemo />;
-}
---
+++ react-native-getting-started/index.js
@@ -0,0 +1,4 @@
+import { registerRootComponent } from 'expo';
+import HelloWorldDemo from './HelloWorldDemo';
+
+registerRootComponent(HelloWorldDemo);
