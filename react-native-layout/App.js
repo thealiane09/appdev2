ece5879a1e7ef56e89149e1ef40c84d6547086b1
thealiane09 <inalealtia15@gmail.com>
2026-02-17 01:22:45 +0800

    Added Signup  screen layout

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Login from './screens/Login';
+import Signup from './screens/Signup';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      <Signup />
     </View>
   );
 }
